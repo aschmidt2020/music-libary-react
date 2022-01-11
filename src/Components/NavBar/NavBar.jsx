cce74a9c4a7965cb10ab891eab080973cde93b61
Audrey Schmidt <audsch@outlook.com>
2022-01-11 14:49:46 -0600

    changed formatting in navbar so that navbar-toggler is functional

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -9,15 +9,15 @@ const NavBar = (props) => {
         <nav className="navbar navbar-expand-lg navbar-light fixed-top" style={{'backgroundColor' : '#e3f2fd'}}>
         <div className="container-fluid">
             <a className="navbar-brand" href="#"><i className="bi bi-music-note-beamed">MusicLibrary</i></a>  
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <div className="nav-item"><SearchBarSingle allSongs={props.allSongs} songs={props.songs} getSpecificSong={props.getSpecificSong}/></div>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarExpand" aria-controls="navbarExpand" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse">
+            <div className="collapse navbar-collapse" id="navbarExpand">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0" style={{'marginLeft':'65%'}}>
                 <li className="nav-item"><FilterMusic navbar='true' allSongs={props.allSongs} songs={props.songs} getFilteredSongs={props.getFilteredSongs} setAllSongs={props.setAllSongs}/></li>
                 <li className="nav-item"><GetAllSongs navbar='true' allSongs={props.allSongs} setAllSongs={props.setAllSongs}/></li>
                 <li className="nav-item"><ScrollToTopButton /></li>
-                <li className="nav-item"><SearchBarSingle allSongs={props.allSongs} songs={props.songs} getSpecificSong={props.getSpecificSong}/></li>
             </ul>
             </div>
         </div>
--- src/Components/SearchBarSingle/SearchBarSingle.jsx
+++ src/Components/SearchBarSingle/SearchBarSingle.jsx
@@ -25,8 +25,8 @@ const SearchBarSingle = (props) => {
     }
 
     return ( 
-        <form className="d-flex" onSubmit={handleSubmit} style={{'marginTop':'1em'}}>
-        <input style={{'marginLeft':'3em'}} className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value)}} />
+        <form className="d-flex" onSubmit={handleSubmit}>
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" value={searchTerm} onChange={(event)=>{setSearchTerm(event.target.value)}} />
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
     );
